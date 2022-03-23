f04c3a12db429b6a3dd8d6eb6cfc478ad0e447b2
Lauro Obed Nolasco Vázquez <18350511@gmail.com>
2022-03-23 17:04:34 -0600

    Input controlado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 const App= () => {
+  // hook -> use.....
+  // state -> useState
+
+  const [state, setState] = useState("");
+
+  
+  const handleInputChange = (event) => { 
+    setState(event.target.value);
+       };
+  
   return (
     <div className="App">
-      <h1> Creación de notas  </h1>
+      <h1> Notas  </h1>
+      <label> Input de prueba
+      <input 
+      id="pruebaID" 
+      name="prueba" 
+      type="text" 
+      onChange={handleInputChange}
+      value={state}
+      />
+      </label>
       </div>
   );
 
