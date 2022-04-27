ca9c3c1e555f06e38374c38ca6856acd569b6dae
Lauro Obed Nolasco Vázquez <18350511@gmail.com>
2022-04-27 14:33:46 -0500

    Creando botón para guardar temporalmente los datos

--- src/App.jsx
+++ src/App.jsx
@@ -16,64 +16,87 @@ const App = () => {
       [event.target.name]: event.target.value,
     });
   };
-  
+
   const handleClickre = () => {
     setInputsState({ /*jjjj*/ ...inputsState, title: "", date: "", note: "" });
   };
 
+  const handleClickSave = () => {
+    localStorage.setItem("notas", JSON.stringify(inputsState))
+  };
+
   return (
     <div className="App container">
-      <div className="row"> 
-      <div className="col">
-        <h3>Lista</h3>
-      </div>
-      <div className="col"> 
-      <h3> Notas </h3>
-      <label className="mb-2">
-        Titulo
-        <input
-          id="title"
-          name="title"
-          type="text"
-          onChange={handleInputChange}
-          value={inputsState.title}
-        />
-      </label>
-      <br />
-      <label className="mb-2">
-        Fecha
-        <input
-          id="date"
-          name="date"
-          type="text"
-          onChange={handleInputChange}
-          value={inputsState.date}
-        />
-      </label>
-      <br />
-      <label>
-        {" "}
-        Nota
-        <input
-          id="note"
-          name="note"
-          type="text"
-          onChange={handleInputChange}
-          value={inputsState.note}
-        />
-      </label>
-      </div>
-      </div>
-      <hr />
-      {/* Botón para hacer reset */}
       <div className="row">
-      <button className="btn btn-primary me-2" onClick={handleClickre}
-        
-        >
-          {/* jan*/}
-          Reset {inputsState.valor}
-        </button>
+        <div className="col">
+          <h3>Lista</h3>
         </div>
+        <div className="col">
+          <h3> Notas </h3>
+          <label className="mb-2" style={{ width: "100%" }}>
+            Titulo
+            <input
+              id="title"
+              name="title"
+              type="text"
+              onChange={handleInputChange}
+              value={inputsState.title}
+              style={{ width: "100%" }}
+            />
+          </label>
+          <br />
+          <label className="mb-2" style={{ width: "100%" }}>
+            Fecha
+            <input
+              id="date"
+              name="date"
+              type="text"
+              onChange={handleInputChange}
+              value={inputsState.date}
+              style={{ width: "100%" }}
+            />
+          </label>
+          <br />
+          <label style={{ width: "100%" }}>
+            {" "}
+            Nota
+            <input
+              id="note"
+              name="note"
+              type="text"
+              onChange={handleInputChange}
+              value={inputsState.note}
+              style={{ width: "100%" }}
+            />
+          </label>
+          <hr />
+          {/* Botón para hacer reset */}
+          <div className="row">
+            <span className="col">
+            <button 
+            type= "button"
+            className="btn btn-primary" onClick={handleClickre}
+            style={{width: "100%"}}>
+              {/* jan*/}
+              Reset {inputsState.valor}
+            </button>
+
+            </span>
+            
+            {/* BOton para guardar */}
+            <span className="col">
+              <button 
+              type= "button"
+              className="btn btn-primary" onClick={handleClickSave} 
+              style={{width: "100%"}}>
+                {/* Botón que sirve para guardar*/}
+                Guardar 
+                
+              </button>{" "}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
